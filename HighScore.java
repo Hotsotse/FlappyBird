384598e7766ee1d994b3e4de1243b8de7e9c2c9a
Hotsotse <hotsotse1@hotmail.com>
2018-06-12 23:20:38 -0400

    HighScore

---
+++ HighScore.java
@@ -0,0 +1,72 @@
+import java.awt.BorderLayout;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class HighScore extends JFrame {
+	private static final long serialVersionUID = 1L;
+	public static int highScore;
+	JLabel scoreLabel;
+
+	public static void main(String[] args) {
+		new HighScore();
+	}
+
+	public HighScore() {
+		super("Score");
+		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		this.setBounds(300, 300, 600, 600);
+		JPanel outer = new JPanel(new BorderLayout());
+
+		scoreLabel = new JLabel();
+		outer.add(scoreLabel);
+		this.setContentPane(outer);
+		loadScore();
+	}
+
+	public HighScore(int score) {
+		this();
+
+	}
+	
+	public static int getScore(int score)
+	{
+		addScore(score);
+		return highScore;
+	}
+
+	public static void addScore(int newScore) {
+
+		if (newScore > highScore) {
+			highScore = newScore;
+		}
+		saveScore();
+	}
+
+	public static void saveScore() {
+		try {
+			PrintWriter out = new PrintWriter("highscore.txt");
+			out.println(highScore);
+			out.close();
+		} catch (IOException e) {
+			System.out.println("Cannot open file (saveScore).");
+		}
+	}
+
+	public void loadScore() {
+		String temp;
+		try {
+			BufferedReader in = new BufferedReader(new FileReader("highscore.txt"));
+			temp = in.readLine(); // Read a line
+			highScore = Integer.parseInt(temp);
+			in.close();
+		} catch (IOException e) {
+			System.out.println("Cannot open file (loadScore).");
+		}
+	}
+}
